8b84751a4b60edf204b50c4ce0df03d9738e34a1
aidilfithroh821-byte <aidilfithroh821@gmail.com>
2026-04-01 23:02:15 +0700

    Add files via upload

---
+++ proyek.py
@@ -0,0 +1,14 @@
+x = int(input("Masukkan jumlah hari: "))
+
+
+tahun = x // 365
+sisa_hari = x % 365
+
+bulan = sisa_hari // 30
+hari = sisa_hari % 30
+
+
+print("Hasil konversi:")
+print("Tahun :", tahun)
+print("Bulan :", bulan)
+print("Hari  :", hari)
